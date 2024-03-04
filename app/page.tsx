6ea36fb73408a25af6a9c2d5349e1b0925a5f32f
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-03-04 19:13:35 +0600

    checked shadcn and clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
+import { SignInButton } from "@clerk/nextjs";
 
 const HomePage = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <SignInButton>Hello</SignInButton>
+      <Button>Hello</Button>
+    </div>
+  );
 };
 
 export default HomePage;
